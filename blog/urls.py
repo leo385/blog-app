65ebb456a8d036b69147dd2760db90fe83291d67
leo385 <leonsoshi@gmail.com>
2024-07-31 14:13:43 -0500

    Create ListView, template of blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "blog"
 urlpatterns = [
 
-    path("", views.index, name="index")
+    path("", views.IndexView.as_view(), name="index")
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from .models import Category, Post
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, it's me, otwieraj drzwi !")
+class IndexView(ListView):
+    
+    template_name = "blog/index.html"
+   
+    def get_queryset(self):
+        pass
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        categories = Category.objects.exclude(name="no category")
+
+        context["categories"] = categories
+        context["posts"] = Post.objects.all()
+
+        return context
+    
